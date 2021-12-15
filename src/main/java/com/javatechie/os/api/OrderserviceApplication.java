81bbb316be4aff6874405460899e1b95ad4f3eb5
anilgitlearn <93112782+anilgitlearn@users.noreply.github.com>
2021-12-15 13:25:56 +0530

    added comment

--- src/main/java/com/javatechie/os/api/OrderserviceApplication.java
+++ src/main/java/com/javatechie/os/api/OrderserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+// added comment
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderserviceApplication {
