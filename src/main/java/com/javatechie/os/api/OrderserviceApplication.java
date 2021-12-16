fa6edf12923772367cc5697a6d7366c68ae1f17c
anilgitlearn <93112782+anilgitlearn@users.noreply.github.com>
2021-12-16 15:37:59 +0530

    Update OrderserviceApplication.java

--- src/main/java/com/javatechie/os/api/OrderserviceApplication.java
+++ src/main/java/com/javatechie/os/api/OrderserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 // added comment
+// from main github to eclipse
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderserviceApplication {
